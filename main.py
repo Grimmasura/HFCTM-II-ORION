0667e5d2fd599d28bbc21ce1d68a604a423706b9
Joshua Humphrey <joshuahumphrey@duck.com>
2025-02-23 18:45:42 -0700

    Delete main.py
    Signed-off-by: Joshua Humphrey <joshuahumphrey@duck.com>

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.orion_model import ORIONModel
-
-if __name__ == "__main__":
-    model = ORIONModel()
-    query = input("Enter your query: ")
-    response = model.infer(query)
-    print("O.R.I.O.N. ∞ Response:", response)
