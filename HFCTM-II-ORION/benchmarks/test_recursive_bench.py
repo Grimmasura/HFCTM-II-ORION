c5d7bcb3802869d34d36955201c4de16fcc09b56
Joshua Humphrey <joshuahumphrey@duck.com>
2025-03-01 22:32:18 -0700

    Delete HFCTM-II-ORION/benchmarks directory
    Signed-off-by: Joshua Humphrey <joshuahumphrey@duck.com>

--- HFCTM-II-ORION/benchmarks/test_recursive_bench.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def test_recursive_infer():
-    url = "http://localhost:8080/api/v1/recursive_infer"
-    data = {"query": "Expand recursive intelligence.", "depth": 0}
-    response = requests.post(url, json=data)
-    assert response.status_code == 200
-
-test_recursive_infer()
